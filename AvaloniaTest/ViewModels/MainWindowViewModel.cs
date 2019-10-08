4b6afadfbbf7c18be8e6df8528d819a2ec6e5aac
PeterAhn <xion486@gmail.com>
2019-10-08 10:01:17 +0900

    img test

--- AvaloniaTest/ViewModels/MainWindowViewModel.cs
+++ AvaloniaTest/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,45 @@
-﻿using System;
+﻿using ReactiveUI;
+using System;
 using System.Collections.Generic;
+using System.Reactive;
 using System.Text;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using Avalonia.Media.Imaging;
+using Avalonia;
+using Avalonia.Platform;
+using System.IO;
+using System.Reflection;
 
 namespace AvaloniaTest.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
+        public MainWindowViewModel()
+        {
+            RunCmd = ReactiveCommand.Create(RunCmdProc);
+        }
+
+        private IBitmap imgSrc;
+        public IBitmap ImageSrc
+        {
+            get => imgSrc;
+            set => this.RaiseAndSetIfChanged(ref imgSrc, value);
+        }
+
+        private void RunCmdProc()
+        {
+            Console.WriteLine("Button Click");
+            string path = $"{Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)}\\Assets\\noimage.jpg";
+            Mat img = new Mat(path);
+            var bmp = img.ToMemoryStream();
+            Bitmap t = new Bitmap(bmp);
+            ImageSrc = t;
+        }
+
         public string Greeting => "Welcome to Avalonia!";
+
+        public ReactiveCommand<Unit, Unit> RunCmd { get; }
+
     }
 }
